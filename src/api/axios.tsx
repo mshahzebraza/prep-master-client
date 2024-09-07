623402f4345f97a78fd74562a36df1bf4ee9c13e
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2024-09-07 14:57:04 +0500

    server url added

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 
+// if we are in development mode, we will use the local server
+const isDevelopment = process.env.NODE_ENV === 'development'
 
-
-const serverAPI = 'http://localhost:3500/api'
+const serverAPI = isDevelopment ? 'http://localhost:3500/api' : 'https://prepmaster-backend.vercel.app/api'
 
 
 export default axios.create({
-    baseURL: serverAPI, // Replace with your base URL
-    // timeout: 5000, // Set a timeout in milliseconds
-  });
+  baseURL: serverAPI, // Replace with your base URL
+  // timeout: 5000, // Set a timeout in milliseconds
+});
 
-  export const axiosPrivate = axios.create({
-    baseURL: serverAPI, // Replace with your base URL
-    headers: {
-      'Authorization': 'Bearer your_token' // Add your authorization token here
-    }
-  });
+export const axiosPrivate = axios.create({
+  baseURL: serverAPI, // Replace with your base URL
+  headers: {
+    'Authorization': 'Bearer your_token' // Add your authorization token here
+  }
+});
 
 
